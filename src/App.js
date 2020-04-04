02ee7cb74e60da7509407f36e3a2b3eedb155fdf
Chin Kuay <kuaychin@gmail.com>
2020-04-04 10:52:15 -0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-const title = "Chin Kuay in Dallas"
-const date = "April 4, 2020"
+const title = "Chin Kuay in Dallas";
+const date = "April 4, 2020";
+const para = "This is a paragraph"
 
 function App() {
   return (
     <div>
       <h1>Hello {title}</h1>
       <h2>{date}</h2>
+      <p>{para}</p>
     </div>
   );
 }
